3f6b5958da25bf5695a7443c8fe0a7f1e182dd5c
YX <1738280880@qq.com>
2020-07-10 10:54:44 +0800

    吃什么？

--- src/main/java/com/zhh/git/Apple.java
+++ src/main/java/com/zhh/git/Apple.java
@@ -3,5 +3,6 @@ package com.zhh.git;
 public class Apple {
     public static void main(String[] args) {
         System.out.println("hot fix !!!!!");
+        System.out.println("吃什么？？？");
     }
 }
